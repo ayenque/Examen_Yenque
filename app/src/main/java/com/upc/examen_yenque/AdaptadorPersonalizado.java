95b2101a839eabf3bb16c49d35b71dc84918164c
Angelo Yenque T <ayenquet@gmail.com>
2021-11-07 02:14:21 -0500

    Implementado el metodo Listar

--- app/src/main/java/com/upc/examen_yenque/AdaptadorPersonalizado.java
+++ app/src/main/java/com/upc/examen_yenque/AdaptadorPersonalizado.java
@@ -1,31 +1,86 @@
 package com.upc.examen_yenque;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.upc.examen_yenque.entidades.Letra;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdaptadorPersonalizado extends RecyclerView.Adapter<AdaptadorPersonalizado.MiHolder> {
+
+    private Context context;
+    private List<Letra> listaLetras = new ArrayList<>();
+
+
+    public AdaptadorPersonalizado(Context context,List<Letra> listaLetras) {
+        this.context = context;
+        this.listaLetras = listaLetras;
+    }
+
     @NonNull
     @Override
     public AdaptadorPersonalizado.MiHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View vista = inflater.inflate(R.layout.fila, parent,false);
+        return new MiHolder(vista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdaptadorPersonalizado.MiHolder holder, int position) {
+        holder.tvSerieLetra.setText(listaLetras.get(position).getSerie()+"");
+        holder.tvCorrelativoLetra.setText(listaLetras.get(position).getCorrelativo()+"");
+        holder.tvrazonSocialLetra.setText(listaLetras.get(position).getRazonSocial()+"");
+        holder.tvnumUnicoLetra.setText(listaLetras.get(position).getNumUnico()+"");
+        holder.tvfecEmisionLetra.setText(listaLetras.get(position).getFecEmision()+"");
+        holder.tvfecVencimientoLetra.setText(listaLetras.get(position).getFecVencimiento()+"");
 
+        holder.btnEditar.setOnClickListener(v -> {
+            Intent intent = new Intent(context,MainActivity.class);
+            intent.putExtra("id",listaLetras.get(position).getId()+"");
+            intent.putExtra("serie",listaLetras.get(position).getSerie()+"");
+            intent.putExtra("correlativo",listaLetras.get(position).getCorrelativo()+"");
+            intent.putExtra("ruc",listaLetras.get(position).getRuc()+"");
+            intent.putExtra("razonSocial",listaLetras.get(position).getRazonSocial()+"");
+            intent.putExtra("numUnico",listaLetras.get(position).getNumUnico()+"");
+            intent.putExtra("fecEmision",listaLetras.get(position).getFecEmision()+"");
+            intent.putExtra("fecVencimiento",listaLetras.get(position).getFecVencimiento()+"");
+            intent.putExtra("comentario",listaLetras.get(position).getComentario()+"");
+            context.startActivity(intent);
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaLetras.size();
     }
 
     public class MiHolder extends RecyclerView.ViewHolder {
+
+        TextView tvSerieLetra, tvCorrelativoLetra,tvrazonSocialLetra,tvnumUnicoLetra;
+        TextView tvfecEmisionLetra,tvfecVencimientoLetra;
+        ImageButton btnEditar,btnEliminar;
+
         public MiHolder(@NonNull View itemView) {
             super(itemView);
+            tvSerieLetra =itemView.findViewById(R.id.tvSerieLetra);
+            tvCorrelativoLetra =itemView.findViewById(R.id.tvCorrelativoLetra);
+            tvrazonSocialLetra =itemView.findViewById(R.id.tvrazonSocialLetra);
+            tvnumUnicoLetra =itemView.findViewById(R.id.tvnumUnicoLetra);
+            tvfecEmisionLetra =itemView.findViewById(R.id.tvfecEmisionLetra);
+            tvfecVencimientoLetra =itemView.findViewById(R.id.tvfecVencimientoLetra);
+            btnEditar =itemView.findViewById(R.id.btnEditar);
+            btnEliminar =itemView.findViewById(R.id.btnEliminar);
+
         }
     }
 }
--- app/src/main/java/com/upc/examen_yenque/MenuActivity.java
+++ app/src/main/java/com/upc/examen_yenque/MenuActivity.java
@@ -2,6 +2,9 @@ package com.upc.examen_yenque;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,29 +15,50 @@ import com.upc.examen_yenque.entidades.Letra;
 import com.upc.examen_yenque.modelo.DAOLetra;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
 
 
     FloatingActionButton btnAgregar;
+    RecyclerView recyclerLetras;
 
+    DAOLetra daoLetra = new DAOLetra(this);
+    List<Letra> listaLetras = new ArrayList<>();
+    AdaptadorPersonalizado adaptadorPersonalizado;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        asignarReferencias();
+        mostrarLetras();
+        daoLetra.abrirBD();
+        mostrarLetras();
+        
+
+    }
+
+    private void mostrarLetras() {
+        listaLetras = daoLetra.listarLetras();
+        adaptadorPersonalizado = new AdaptadorPersonalizado(this,listaLetras);
+        recyclerLetras.setAdapter(adaptadorPersonalizado);
+        recyclerLetras.setLayoutManager(new LinearLayoutManager(MenuActivity.this));
+
+    }
+
+    private void asignarReferencias() {
+        recyclerLetras = findViewById(R.id.recyclerLetras);
 
         btnAgregar = findViewById(R.id.btnAgregar);
         btnAgregar.setOnClickListener(v->{
             Intent intent = new Intent(MenuActivity.this,MainActivity.class);
             startActivity(intent);
         });
-        
-
     }
-
 
 
 
